163666cc673e2f529fccc4c7ceb20301269ffb48
Rohan Sakhare <126242412+rohsak5649@users.noreply.github.com>
2024-06-22 13:05:05 +0530

    Create  7.Write a C++ program to sort characters (numbers and punctuation symbols are not included) in a string.cpp

---
+++ 7.Write a C++ program to sort characters (numbers and punctuation symbols are not included) in a string.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string sort_char(string text) {
+    bool flag;
+    char ch;
+    int n = text.length();
+
+    do {
+        flag = false;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (text[i] > text[i + 1]) {
+                ch = text[i];
+                text[i] = text[i + 1];
+                text[i + 1] = ch;
+                flag = true;
+            }
+        }
+
+    } while (flag);
+
+    string str;
+    for (int j = 0; j < n; j++) {
+        if (text[j] != ' ') {
+            str.push_back(text[j]);
+        }
+    }
+    return str;
+}
+
+int main() {
+    cout << sort_char("rohan") << endl;
+    return 0;
+}
